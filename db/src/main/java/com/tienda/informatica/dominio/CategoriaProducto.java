1aca99929ebf3a6fcef54c94390b0644bfcde275
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-11-16 11:47:59 -0500

    Entidad CategoriaProducto

---
+++ db/src/main/java/com/tienda/informatica/dominio/CategoriaProducto.java
@@ -0,0 +1,31 @@
+package main.java.com.tienda.informatica.dominio;
+
+public class CategoriaProducto {
+    private int idCategoria;
+    private String nombre;  
+
+    public CategoriaProducto() {
+    }
+
+    public CategoriaProducto(int idCategoria, String nombre) {
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+    }
+
+    //Getters y Setters
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
