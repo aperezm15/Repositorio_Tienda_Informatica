952fc6e4185ba3fb5611b6662836d05d4b944a49
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-11-21 13:47:36 -0500

    Update ProductoRepositoryPort.java

--- NetBeansProjects/tienda_informatica_final/src/main/java/ports/ProductoRepositoryPort.java
+++ NetBeansProjects/tienda_informatica_final/src/main/java/ports/ProductoRepositoryPort.java
@@ -6,15 +6,15 @@ import model.Producto;
 
 public interface ProductoRepositoryPort {
 
-    int guardarProducto(Producto producto); // Método para guardar un producto.
+    int guardarProducto(Producto producto); 
 
-    Producto buscarProductoPorId(int id); // Método para buscar un producto por su ID.
+    Producto buscarProductoPorId(int id); 
 
-    List<Producto> obtenerTodosLosProductos(); // Método para obtener todos los productos.
+    List<Producto> obtenerTodosLosProductos(); 
 
-    boolean actualizarProducto(Producto producto); // Método para actualizar un producto.
+    boolean actualizarProducto(Producto producto); 
 
-boolean eliminarProducto(int id); // Método para eliminar un producto por su ID.
+boolean eliminarProducto(int id); 
 
 
 
